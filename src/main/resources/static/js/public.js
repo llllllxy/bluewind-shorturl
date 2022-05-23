591733937078e43a192759e4802a1c65561e6515
liuxingyu01 <184974699@qq.com>
2022-05-23 10:21:48 +0800

    入库coco-message提醒库

--- src/main/resources/static/js/public.js
+++ src/main/resources/static/js/public.js
@@ -1,53 +1,136 @@
 
 
-// 创建一个axios实例
-const $axios = axios.create({
-    // 设置超时时间
-    timeout: 30000,
-})
-
-// 请求拦截器
-$axios.interceptors.request.use(config => {
-        if (config.method === 'get') { // 如果是get请求的话，则拼接上时间戳，防止浏览器缓存
-            if (config.url.indexOf('?') === -1) {
-                config.url = config.url + "?t=" + new Date().getTime();
-            } else {
-                config.url = config.url + "&t=" + new Date().getTime();
-            }
-        } else if (config.method === 'post') { // 如果是post请求的话，则根据请求头自动转换请求格式
-            console.log('Content-Type = ' + config.headers['Content-Type']);
-            if (config.headers['Content-Type'] === 'application/x-www-form-urlencoded; charset=UTF-8') {
-                config.data = Qs.stringify(config.data);
-            } else {
-                config.data = JSON.stringify(config.data);
-            }
+/**
+ * 设置AJAX的全局默认选项，
+ * 当AJAX请求会话过期时，跳转到登陆页面
+ */
+$.ajaxSetup({
+    complete: function(XMLHttpRequest, textStatus){
+        if (XMLHttpRequest.status == 302) {
+            layer.alert('会话已过期，请重新登录', function(index){
+                layer.close(index);
+                window.location.href = Util.ctx + "admin/login";
+            });
         }
-
-        return config
-    }, error => {
-        return Promise.reject(error)
     }
-)
+} );
 
-// 响应拦截器
-$axios.interceptors.response.use(
-    response => {
-        // 获取请求状态码
-        const code = response.data.code;
-        if (code === 401) {
-            console.log("axios.interceptors.response -- code = " + code);
-            if (confirm("会话已失效，是否重新登录")) {
-                // 跳转到登陆页面
-                window.location.href = Util.ctx + "login";
-            }
-            return Promise.reject(response);
-        } else {
-            return response;
+
+/**
+ * 配置layer.open的宽度和高度
+ */
+const layerwidth = 800;
+const layerheight = ($(window).height() - 50);
+
+
+/**
+ * Ajax请求封装
+ */
+const AjaxUtil = {
+    ctx: /*[[@{/}]]*/'',
+
+    /**
+     * GET 请求
+     */
+    get: function (options) {
+        if (!options.url) {
+            alert('请求错误，url不可为空!');
+            return false;
         }
+        options.type = 'GET';
+        options.timeout = options.timeout || 5000; // 设置本地的请求超时时间（以毫秒计）
+        options.async = options.async || true; // 布尔值，表示请求是否异步处理。默认是 true
+        options.cache = options.cache || false; // 布尔值，表示浏览器是否缓存被请求页面，默认是true
+        options.dataType = options.dataType || 'json';
+        $.ajax({
+            url: options.url,
+            type: options.type,
+            timeout: options.timeout,
+            async: options.async,
+            cache: options.cache,
+            dataType: options.dataType,
+            success: function (data, textStatus, jqXHR) {
+                // 成功回调
+                options.success(data);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                // 错误回调
+                options.error("错误提示： " + XMLHttpRequest.status + " " + XMLHttpRequest.statusText);
+            }
+        });
     },
-    error => {
-        return Promise.reject(error)
+
+
+    /**
+     * POST 请求
+     */
+    post: function (options) {
+        if (!options.url) {
+            alert('请求错误，url不可为空!');
+            return false;
+        }
+        options.type = 'POST';
+        options.timeout = options.timeout || 5000;
+        options.async = options.async || true;
+        options.cache = options.cache || false;
+        options.dataType = options.dataType || 'json';
+        options.contentType = options.contentType || 'application/x-www-form-urlencoded';
+        options.data = options.data || '';
+        $.ajax({
+            url: options.url,
+            type: options.type,
+            timeout: options.timeout,
+            async: options.async,
+            cache: options.cache,
+            dataType: options.dataType,
+            data: options.data,
+            contentType: options.contentType, // 发送数据到服务器时所使用的内容类型。默认是："application/x-www-form-urlencoded"
+            success: function (data, textStatus, jqXHR) {
+                // 成功回调
+                options.success(data);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                // 错误回调
+                options.error("错误提示： " + XMLHttpRequest.status + " " + XMLHttpRequest.statusText);
+            }
+        });
+    },
+
+
+    /**
+     * upload 文件上传 请求
+     */
+    upload: function (options) {
+        if (!options.url) {
+            alert('请求错误，url不可为空!');
+            return false;
+        }
+        options.type = 'POST';
+        options.timeout = options.timeout || 5000;
+        options.async = options.async || true;
+        options.cache = options.cache || false;
+        options.dataType = options.dataType || 'json';
+        options.contentType = options.contentType || false;
+        options.processData = options.processData || false;
+        options.data = options.data || new FormData();
+        $.ajax({
+            url: options.url,
+            type: options.type,
+            timeout: options.timeout,
+            async: options.async,
+            cache: options.cache,
+            dataType: options.dataType,
+            processData: options.processData,
+            data: options.data,
+            contentType: options.contentType,
+            success: function (data, textStatus, jqXHR) {
+                // 成功回调
+                options.success(data);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                // 错误回调
+                options.error("错误提示： " + XMLHttpRequest.status + " " + XMLHttpRequest.statusText);
+            }
+        });
     }
-)
-
-
+}
