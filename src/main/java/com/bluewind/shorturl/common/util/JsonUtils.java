0dd4155bfbc812da1460a9809901ac3bb2861895
liuxingyu01 <184974699@qq.com>
2022-06-08 10:14:50 +0800

    修改注释

--- src/main/java/com/bluewind/shorturl/common/util/JsonUtils.java
+++ src/main/java/com/bluewind/shorturl/common/util/JsonUtils.java
@@ -3,56 +3,133 @@ package com.bluewind.shorturl.common.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author liuxingyu01
  * @date 2022-03-11-16:47
  **/
 public class JsonUtils {
+    final static Logger log = LoggerFactory.getLogger(JsonUtils.class);
 
+    /**
+     * 对象转JSON字符串
+     * @param value
+     * @return
+     */
     public static String writeValueAsString(Object value) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.writeValueAsString(value);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return "";
+        if (value != null) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.writeValueAsString(value);
+            } catch (JsonProcessingException e) {
+                if (log.isErrorEnabled()) {
+                    log.error("JsonUtils -- writeValueAsString -- Exception=", e);
+                }
+            }
         }
+        return "";
     }
 
+    /**
+     * JSON字符串转对象
+     * @param content
+     * @param valueType
+     * @param <T>
+     * @return
+     */
     public static <T> T readValue(String content, Class<T> valueType) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.readValue(content, valueType);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+        if (content != null && !content.trim().isEmpty()) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue(content, valueType);
+            } catch (IOException e) {
+                if (log.isErrorEnabled()) {
+                    log.error("JsonUtils -- readValue -- Exception=", e);
+                }
+            }
         }
+
+        return null;
     }
 
+    /**
+     * JSON字符串转对象
+     * @param content
+     * @param valueTypeRef
+     * @param <T>
+     * @return
+     */
     public static <T> T readValue(String content, TypeReference<T> valueTypeRef) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.readValue(content, valueTypeRef);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+        if (content != null && !content.trim().isEmpty()) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue(content, valueTypeRef);
+            } catch (IOException e) {
+                if (log.isErrorEnabled()) {
+                    log.error("JsonUtils -- readValue -- Exception=", e);
+                }
+            }
         }
+        return null;
     }
 
+
+    /**
+     * InputStream文件流转对象
+     * @param src
+     * @param valueType
+     * @param <T>
+     * @return
+     */
     public static <T> T readValue(InputStream src, Class<T> valueType) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.readValue(src, valueType);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+        if (src != null) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue(src, valueType);
+            } catch (IOException e) {
+                if (log.isErrorEnabled()) {
+                    log.error("JsonUtils -- readValue -- Exception=", e);
+                }
+            }
         }
+        return null;
     }
 
+    /**
+     * JSON字符串转List
+     * @param content
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> readArrayValue(String content, Class<T> clazz) {
+        if (content != null && !content.trim().isEmpty()) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue(content, objectMapper.getTypeFactory().constructParametricType(ArrayList.class, clazz));
+            } catch (Exception e) {
+                if (log.isErrorEnabled()) {
+                    log.error("JsonUtils -- readArrayValue -- Exception=", e);
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 类型转换
+     * @param fromValue
+     * @param toValueType
+     * @param <T>
+     * @return
+     */
     public static <T> T convertValue(Object fromValue, Class<T> toValueType) {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.convertValue(fromValue, toValueType);
