988250bd96aeb5426929021929896fc173a43476
liuxingyu01 <184974699@qq.com>
2022-06-23 11:15:11 +0800

    增加isURL公共方法，用于检验是否是个正确的网址

--- src/main/resources/static/js/public.js
+++ src/main/resources/static/js/public.js
@@ -25,6 +25,31 @@ function toLine(name) {
 
 
 /**
+ * 检验是否是个正确的网址
+ * @param str_url
+ * @returns {boolean}
+ */
+function isURL(str_url) {
+    var strRegex =
+        '^((https|http|ftp|rtsp|mms)?://)' +
+        "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" + // ftp的user@
+        '(([0-9]{1,3}.){3}[0-9]{1,3}' + // IP形式的URL- 199.194.52.184
+        '|' + // 允许IP和DOMAIN（域名）
+        "([0-9a-z_!~*'()-]+.)*" + // 域名- www.
+        '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' + // 二级域名
+        '[a-z]{2,6})' + // first level domain- .com or .museum
+        '(:[0-9]{1,4})?' + // 端口- :80
+        '((/?)|' + // a slash isn't required if there is no file name
+        "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+    var re = new RegExp(strRegex);
+    if (re.test(str_url)) {
+        return true;
+    }
+    return false;
+};
+
+
+/**
  * bootstrapTable封装，减少公共配置项
  * @param options
  */
