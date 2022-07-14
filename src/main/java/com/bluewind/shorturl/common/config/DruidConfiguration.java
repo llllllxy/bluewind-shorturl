16919d265fb6cb5d3006bbc5e0791de0f1aef1b7
liuxingyu01 <184974699@qq.com>
2022-07-14 17:00:07 +0800

    优化druid的配置

--- src/main/java/com/bluewind/shorturl/common/config/DruidConfiguration.java
+++ src/main/java/com/bluewind/shorturl/common/config/DruidConfiguration.java
@@ -24,22 +24,6 @@ public class DruidConfiguration {
 
 
     @Bean
-    public ServletRegistrationBean druidServlet() {
-        logger.info("init Druid Servlet Configuration ");
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
-        // IP白名单
-//        servletRegistrationBean.addInitParameter("allow", "*");
-        // IP黑名单(共同存在时，deny优先于allow)
-//        servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
-        //控制台管理用户
-        servletRegistrationBean.addInitParameter("loginUsername", "admin"); // 用户名
-        servletRegistrationBean.addInitParameter("loginPassword", "123456"); // 密码
-        //是否能够重置数据 禁用HTML页面上的“Reset All”功能
-        servletRegistrationBean.addInitParameter("resetEnable", "false");
-        return servletRegistrationBean;
-    }
-
-    @Bean
     public FilterRegistrationBean filterRegistrationBean() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
         filterRegistrationBean.addUrlPatterns("/*");
