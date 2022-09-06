f7fdfb00bf642b0137b647353e1911e2073afd88
liuxingyu01 <184974699@qq.com>
2022-09-06 10:24:45 +0800

    feat: 增加几个web相关的公共方法

---
+++ src/main/java/com/bluewind/shorturl/common/util/web/ResponseUtil.java
@@ -0,0 +1,29 @@
+package com.bluewind.shorturl.common.util.web;
+
+import com.bluewind.shorturl.common.util.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author liuxingyu01
+ * @date 2021-01-30-11:21
+ **/
+public class ResponseUtil {
+    private final static Logger logger = LoggerFactory.getLogger(ResponseUtil.class);
+
+    public static void sendJson(ServletResponse response, Object responseObject) {
+        // 将实体对象转换为JSON Object转换
+        String jsonStr = JsonUtils.writeValueAsString(responseObject);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.append(jsonStr);
+        } catch (IOException e) {
+            logger.error("ResponseUtil -- sendJson -- IOException = {e}", e);
+        }
+    }
+}
