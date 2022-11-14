636407aff520d3286706c3f0af0172c0e6046e03
liuxingyu01 <184974699@qq.com>
2022-11-14 15:32:47 +0800

    调用结束后删除TRACE_ID，参考https://juejin.cn/post/7074461710030995492

--- src/main/java/com/bluewind/shorturl/common/config/logtracking/TraceIdInterceptor.java
+++ src/main/java/com/bluewind/shorturl/common/config/logtracking/TraceIdInterceptor.java
@@ -2,6 +2,7 @@ package com.bluewind.shorturl.common.config.logtracking;
 
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,10 +16,24 @@ import java.util.UUID;
  **/
 @Component
 public class TraceIdInterceptor extends HandlerInterceptorAdapter {
+    public static final String TRACE_ID = "traceId";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String traceId = UUID.randomUUID().toString().replace("-", "");
-        MDC.put("traceId", traceId);
+        MDC.put(TRACE_ID, traceId);
         return true;
     }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        // 调用结束后删除，参考https://juejin.cn/post/7074461710030995492
+        MDC.remove(TRACE_ID);
+    }
+
 }
